8e76e7207ca49242f78f68bbbd72f4fa2861fea0
BotuGamer07 <158021193+BotuGamer07@users.noreply.github.com>
2024-01-29 22:11:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ client.on('ready', async () => {
     .setName('BOTU GAMER')
     .setDetails(`THE NAME IT SHOWS YOUR STREAMING [${formatTime()}]`)
     .setStartTimestamp(Date.now())
- .setAssetsLargeImage('https://media.discordapp.net/attachments/1191074543469592597/1201416511252865055/standard_10.gif?ex=65c9bd51&is=65b74851&hm=8342543ae7a2cdd6aac899534b85502f3ce65192dc8b9172ece5303918188a90&') //You can put links in tenor or discord and etc.
+ .setAssetsLargeImage('https://media.discordapp.net/attachments/1200845159169015828/1201567554070650950/a_251d4e9c5504e98fa0d56832dc55da94.gif?ex=65ca49fc&is=65b7d4fc&hm=e9c79ed81bc91e1f8b338fc2e1638190e103bd83708d9b6a78c92dfc81177948&') //You can put links in tenor or discord and etc.
     .setAssetsLargeText('Botu') //Text when you hover the Large image
     .setAssetsSmallImage('https://media.discordapp.net/attachments/1191074543469592597/1201418095185641532/20240115_073430.gif?ex=65c9becb&is=65b749cb&hm=27e98260be09cb8ab720ce2961ed2db3b66917333a3a26f10a959d59df22fccc&') //You can put links in tenor or discord and etc.
     .setAssetsSmallText('op') //Text when you hover the Small image
